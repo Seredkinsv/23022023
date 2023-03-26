f6182691aff64c64aa1049c9509739aae9e51cb3
Seredkinsv <ss20003@ya.ru>
2023-03-26 10:40:03 +0300

    init

---
+++ seminars/seminar1/projectdz2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа принимает 3 числа и выдает какое число больше - max
+
+//Получение входных чисел от пользователя и присвоение их в программу
+Console.Write("Введи первое целое число: ");
+
+int numbera = int.Parse(Console.ReadLine());
+
+Console.Write("Введи второе целое число: ");
+
+int numberb = int.Parse(Console.ReadLine());
+
+Console.Write("Введи третье целое число: ");
+
+int numberc = int.Parse(Console.ReadLine());
+
+int max_num = numbera;
+
+// Сравнение входных чисел - так и не решил
+
+if(numberb > max_num)
+{
+    Console.Write("max - второе число");
+    }
+if(numberc > numbera)
+{
+    Console.Write("max - третье число");
+}
+else
+{
+    Console.WriteLine("первое число - мах");
+}
