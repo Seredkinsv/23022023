4c96e3ec682fca26255e03ab601af28c4b91a1e3
Seredkinsv <ss20003@ya.ru>
2023-04-02 12:42:02 +0300

    init11

---
+++ seminars/seminar3/dzkseminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿// while
+//создаем переменную - счетчик
+//int count = 0;
+//while (count < 10)
+//{
+  //  Console.WriteLine($"{count}.Hello");
+//count++;
+//}
+
+ //Console.WriteLine("Выход из цикла");
+
+ //for
+ //for (int i = 0; i < 10; i++)
+ //{
+   // Console.WriteLine($"{count}.Hello");
+ //}
+
+  //Console.WriteLine("Выход из цикла");
